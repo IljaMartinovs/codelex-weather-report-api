6042f8bfb85c7ec5b579011621a3953364d37946
Iļja Martinovs <martinovsilja@gmail.com>
2022-11-19 13:41:27 +0200

    Added

---
+++ Weather Report/CityCollection.php
@@ -0,0 +1,17 @@
+<?php
+
+class CityCollection
+{
+    private City $cities ;
+
+    public function __construct(City $cities)
+    {
+        $this->cities = $cities;
+    }
+
+    public function getAllData(): string
+    {
+       return $this->cities->getCity() . " | " . $this->cities->getTemperature() . " | " . $this->cities->getWeather()  . " | " . $this->cities->getWind() .  "\n";
+    }
+
+}
---
+++ Weather Report/index.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once "City.php";
+require_once "Api.php";
+require_once "CityCollection.php";
+$collection = [];
+
+while(true) {
+    echo "[1] Get data about city :\n";
+    echo "[2] Get all saved data about cities :\n";
+    $input = readline();
+    switch ($input) {
+
+        case 1 :
+            $name = (readline("Enter a city : "));
+            $apiKey = "Insert your key here";
+            $api = new Api($apiKey,$name);
+            $city = new City($name, $api->getData()[0], $api->getData()[1], $api->getData()[2]);
+            $collection [] = new CityCollection($city);
+            echo "City : {$city->getCity()}\n";
+            echo "Temperature : {$city->getTemperature()}\n";
+            echo "Weather : {$city->getWeather()}\n";
+            echo "Wind speed : {$city->getWind()}\n";
+            break;
+
+        case 2 :
+            foreach ($collection as $city){
+                echo $city->getAllData();
+            }
+            break;
+
+        default:
+            exit;
+    }
+}
